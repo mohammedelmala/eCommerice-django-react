b1d5c75376f181accb559a258795b3abfdf0d42f
mohammedelmala <mala_moh@msn.com>
2021-03-21 06:25:13 +0400

    update backend

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import status
 
+from base.models import Order, ShippingAddress, OrderItem, Product
+
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
@@ -16,10 +18,37 @@ def addOrderItems(request):
         return Response({"detail":"No order items","status":status.HTTP_400_BAD_REQUEST})
     else:
         # (1) create order
+        order = Order.objects.create(user=user,
+        paymentMethod=data['paymentMethod'],
+        taxPrice = data['taxPrice'],
+        shippingPrice = data['shippingPrice'],
+        totalPrice = data['totalPrice']
+        )
 
         # (2) create shipping address
 
+        shippingAddress = ShippingAddress.objects.create(order = order ,
+        address=data['shippingAddress']['address'],
+        city= data['shippingAddress']['city'],
+        postalCode= data['shippingAddress']['postalCode'],
+        country= data['shippingAddress']['country'],
+        shippingPrice=data['shippingPrice']
+
+        )
+
+
         # (3) create order items
+
+        for item in orderItems:
+            product = Product.objects.get(id=item.product)
+            orderItem = OrderItem.objects.create(
+            product = product,
+            user = user,
+            name = item.name,
+            qty = item.qty,
+            price = item.price,
+            image = item.image
+            )
 
         # (4) update stock
 
